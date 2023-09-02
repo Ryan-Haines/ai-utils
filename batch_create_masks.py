91aa82f62f286adec1a377fe16ab16b14729e76b
Ryan Haines <ryan.a.haines@gmail.com>
2023-09-02 17:12:13 -0400

    add min and max area to ignore bad detections

--- batch_create_masks.py
+++ batch_create_masks.py
@@ -15,7 +15,7 @@ def is_touching_corners(x1, y1, x2, y2, img_width, img_height, xpad, ypad):
     return False
 
 
-def process_image(image_path, out_folder, include_textfile, xpad_detect, ypad_detect, xpad_box, ypad_box, corners=False, edges=False, only_largest=False, overwrite=False):
+def process_image(image_path, out_folder, include_textfile, xpad_detect=None, ypad_detect=None, xpad_box=0, ypad_box=0, corners=False, edges=False, only_largest=False, overwrite=False, min_area=1, max_area=10):
     mask_filename = os.path.join(out_folder, os.path.basename(image_path))
     if os.path.exists(mask_filename) and not overwrite:
         print(f"Mask already exists for {os.path.basename(image_path)}, skipping.")
@@ -35,21 +35,28 @@ def process_image(image_path, out_folder, include_textfile, xpad_detect, ypad_de
         ypad_detect = int(height * 0.05)
     
     mask_created = False
-    max_area = 0
     largest_detection = None
     
+    total_area = width * height  
+
     for detection in result:
         top_left, top_right, bottom_right, bottom_left = detection[0]
         x1, y1 = top_left
         x2, y2 = bottom_right
         area = (x2 - x1) * (y2 - y1)
+        print(f"Area: {area}")
+
+        area_percent = (area / total_area) * 100
+        if area_percent < min_area or area_percent > max_area:
+            print(f"Area percentage {area_percent} is not within the range of {min_area} and {max_area}.")
+            continue
 
         should_draw = True
-        
         if edges:
             should_draw = is_touching_edges(x1, y1, x2, y2, width, height, xpad_detect, ypad_detect)
         elif corners:
             should_draw = is_touching_corners(x1, y1, x2, y2, width, height, xpad_detect, ypad_detect)
+            
 
         if should_draw:
             if only_largest:
@@ -57,13 +64,21 @@ def process_image(image_path, out_folder, include_textfile, xpad_detect, ypad_de
                     max_area = area
                     largest_detection = detection
             else:
-                draw.rectangle([x1 - xpad_box, y1 - ypad_box, x2 + xpad_box, y2 + ypad_box], fill="black")
+                x1_draw = max(x1 - xpad_box, 0)
+                y1_draw = max(y1 - ypad_box, 0)
+                x2_draw = min(x2 + xpad_box, width)
+                y2_draw = min(y2 + ypad_box, height)
+                draw.rectangle([x1_draw, y1_draw, x2_draw, y2_draw], fill="black")
                 mask_created = True
-    
+
     if only_largest and largest_detection:
         x1, y1 = largest_detection[0][0]
         x2, y2 = largest_detection[0][2]
-        draw.rectangle([x1 - xpad_box, y1 - ypad_box, x2 + xpad_box, y2 + ypad_box], fill="black")
+        x1_draw = max(x1 - xpad_box, 0)
+        y1_draw = max(y1 - ypad_box, 0)
+        x2_draw = min(x2 + xpad_box, width)
+        y2_draw = min(y2 + ypad_box, height)
+        draw.rectangle([x1_draw, y1_draw, x2_draw, y2_draw], fill="black")
         mask_created = True
 
     if mask_created:
@@ -88,6 +103,9 @@ def main():
     parser.add_argument('--ypad-detect', type=int, default=None, help='Vertical padding for detection.')
     parser.add_argument('--xpad-box', type=int, default=0, help='Horizontal padding for bounding box.')
     parser.add_argument('--ypad-box', type=int, default=0, help='Vertical padding for bounding box.')
+    parser.add_argument('--min-area', type=float, default=1, help='Minimum area as a percentage to include bounding box. Default is 1%.')
+    parser.add_argument('--max-area', type=float, default=10, help='Maximum area as a percentage to include bounding box. Default is 10%.')
+
     args = parser.parse_args()
     
     if not os.path.exists(args.out):
@@ -99,7 +117,7 @@ def main():
 
     for filename in tqdm(image_files, desc="Processing images"):
         full_path = os.path.join(args.path, filename)
-        process_image(full_path, args.out, args.include_textfile, args.xpad_detect, args.ypad_detect, args.xpad_box, args.ypad_box, args.corners, args.edges, args.only_largest, args.overwrite)
+        process_image(full_path, args.out, args.include_textfile, args.xpad_detect, args.ypad_detect, args.xpad_box, args.ypad_box, args.corners, args.edges, args.only_largest, args.overwrite, args.min_area, args.max_area)
 
 if __name__ == "__main__":
     main()
